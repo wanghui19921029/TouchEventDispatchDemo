20a33c589bf9e75cf48eb4928dc100781652cd11
wanghui1 <wanghui1@enovatemotors.com>
2019-10-25 11:16:22 +0800

    TouchEventDemo: init code_2.0
    Signed-off-by: wanghui1 <wanghui1@enovatemotors.com>

--- app/src/main/java/com/example/wh/toucheventdispatchdemo/MainActivity.java
+++ app/src/main/java/com/example/wh/toucheventdispatchdemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -14,40 +16,49 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private BaseRelativeLayout mRelativeLayout;
     private BaseLinearLayout mLinearLayout;
     private TextView mTextView;
+    private ImageView mImageView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFrameLayout = findViewById(R.id.framelayout);
+        mRelativeLayout = findViewById(R.id.relativelayout);
+        mLinearLayout = findViewById(R.id.linearlayout);
+        mTextView = findViewById(R.id.textview);
+        mImageView = findViewById(R.id.imageview);
+        mButton = findViewById(R.id.button);
 
-        mFrameLayout = (BaseFrameLayout) findViewById(R.id.fl);
-        mRelativeLayout = (BaseRelativeLayout) findViewById(R.id.rl);
-        mLinearLayout = (BaseLinearLayout) findViewById(R.id.ll);
-
-        mTextView = (TextView) findViewById(R.id.textview);
-        mTextView.setOnClickListener(this);
-
-        // 注释掉和放开,结果是不一样的,可以看log加深理解
-        // mFrameLayout.setOnClickListener(this);
-        // mRelativeLayout.setOnClickListener(this);
-        // mLinearLayout.setOnClickListener(this);
-
+        //注释掉和放开,结果是不一样的,可以看log加深理解
+        //mFrameLayout.setOnClickListener(this);
+        //mRelativeLayout.setOnClickListener(this);
+//        mLinearLayout.setOnClickListener(this);
+        //mTextView.setOnClickListener(this);
+        //mImageView.setOnClickListener(this);
+        //mButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.fl:
-                Log.i(TAG, "onClick: fl");
+            case R.id.framelayout:
+                Log.i(TAG, "onClick: framelayout");
                 break;
-            case R.id.rl:
-                Log.i(TAG, "onClick: rl");
+            case R.id.relativelayout:
+                Log.i(TAG, "onClick: relativelayout");
                 break;
-            case R.id.ll:
-                Log.i(TAG, "onClick: ll");
+            case R.id.linearlayout:
+                Log.i(TAG, "onClick: linearlayout");
                 break;
             case R.id.textview:
                 Log.i(TAG, "onClick: textview");
+                break;
+            case R.id.imageview:
+                Log.i(TAG, "onClick: imageview");
+                break;
+            case R.id.button:
+                Log.i(TAG, "onClick: button");
                 break;
             default:
                 break;
